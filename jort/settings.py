0c9de090cfbdaf39b1b5519b7906aeb2d55905bd
Justin Kaseman <justinkaseman@live.com>
2018-06-28 13:01:06 -0700

    token auth

--- jort/settings.py
+++ jort/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'notes',
     'bookmarks',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -123,10 +124,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
+
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
 }
