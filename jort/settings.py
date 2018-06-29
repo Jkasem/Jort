0564c31c8745b6571e28cd6b1c9f067d8fcaf9d7
Justin Kaseman <justinkaseman@live.com>
2018-06-29 09:46:25 -0700

    commas

--- jort/settings.py
+++ jort/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = ‘whitenoise.storage.CompressedManifestStaticFilesStorage’
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
 
 REST_FRAMEWORK = {
